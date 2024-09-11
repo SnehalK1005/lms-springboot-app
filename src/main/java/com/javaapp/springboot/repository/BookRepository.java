a2549d92cd1f1cb5804bae1f7793e4407230d482
SnehalK1005 <snehal.s.kotian@gmail.com>
2024-09-11 12:38:53 +0530

    Search Book by title author and publisher Rest API

--- src/main/java/com/javaapp/springboot/repository/BookRepository.java
+++ src/main/java/com/javaapp/springboot/repository/BookRepository.java
@@ -7,6 +7,11 @@ import com.javaapp.springboot.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleContaining(String title);
+    List<Book> findByAuthorContaining(String author);
+    List<Book> findByPublisherContaining(String publisher);
 }
